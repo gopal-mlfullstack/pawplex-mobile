d182647ec4a5e0baddbc5bccfa0d0de2fa7834b5
Gopal Mahato <gopalmahatomain2026@gmail.com>
2026-03-19 20:24:08 +0530

    Update api

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,13 +2,13 @@ import API from "./axios";
 
 // Register user
 export const register = async (data) => {
-  const res = await API.post("/api/auth/register/", data);
+  const res = await API.post("/auth/register/", data);
   return res.data;
 };
 
 // Login user
 export const login = async (data) => {
-  const res = await API.post("/api/auth/login/", data);
+  const res = await API.post("/auth/login/", data);
 
   localStorage.setItem("access_token", res.data.access);
   localStorage.setItem("refresh_token", res.data.refresh);
@@ -18,19 +18,19 @@ export const login = async (data) => {
 
 // Get logged-in user
 export const getProfile = async () => {
-  const res = await API.get("/api/auth/me/");
+  const res = await API.get("/auth/me/");
   return res.data;
 };
 
 // Update user profile
 export const updateProfile = async (data) => {
-  const res = await API.put("/api/auth/me/", data);
+  const res = await API.put("/auth/me/", data);
   return res.data;
 };
 
 // Change password
 export const changePassword = async (data) => {
-  const res = await API.post("/api/auth/change-password/", data);
+  const res = await API.post("/auth/change-password/", data);
   return res.data;
 };
 
